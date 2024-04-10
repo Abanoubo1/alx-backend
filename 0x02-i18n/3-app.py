1a3925cc2aed80828a9919ee5d72b3765172ce0e
Abanoubo1 <abanoubwageh88@gmail.com>
2024-04-10 03:21:42 +0200

    rep 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,43 +1,12 @@
 #!/usr/bin/env python3
-
+"""A Basic Flask app with internationalization support.
 """
-2-app.py
-Module that starts a Flask web application with Babel
-integration and locale selection
-
-Imports:
-    flask.Flask to setup the web application
-    flask.render_template to render the HTML files
-    flask.request to access request data
-    flask_babel.Babel for internationalization support
-
-Classes:
-    Config: Configuration class for Babel
-
-Functions:
-    get_locale: Determines the best language match from client's
-    Accept Languages header
-    index: Returns the rendered template for the '/' route
-
-App Routing:
-    @app.route('/') -> Calls the index function to return the HTML page
-
-Running:
-    The application runs on host 0.0.0.0 and port 5000
-"""
-
-from flask import Flask, render_template, request
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
-class Config(object):
-    """
-    Configuration class for Babel
-    Attributes:
-        LANGUAGES (list): The list of languages supported
-        by the web application
-        BABEL_DEFAULT_LOCALE (str): The default language locale
-        BABEL_DEFAULT_TIMEZONE (str): The default timezone
+class Config:
+    """Represents a Flask Babel configuration.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -46,32 +15,23 @@ class Config(object):
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
     """
-    get_locale function
-    Returns:
-        The best language match from client's Accept
-        Languages header
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
-def index():
-    """
-    index function
-    Returns:
-        Rendered template for the '/' route
+def get_index() -> str:
+    """The home/index page.
     """
     return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    """
-    MAIN App
-    """
     app.run(host='0.0.0.0', port=5000)
